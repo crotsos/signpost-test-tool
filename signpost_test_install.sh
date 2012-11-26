118d8173401b38f4e0ac08bc951d4097732d2bf8
crotsos <cr409@cl.cam.ac.uk>
2012-11-26 17:17:49 +0000

    adding installation string

---
+++ signpost_test_install.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash 
+
+set -x 
+
+dir=`pwd`
+mkdir tmp
+cd tmp
+
+
+if [ ! $(which opam) ]; then
+
+  echo "fetch and install opam... "
+  git clone git://github.com/OCamlPro/opam.git
+  cd opam
+  ./configure --prefix=$dir
+  make all install 
+  cd ..
+  PATH=$dir/bin/:$PATH
+fi
+
+echo "init the repo...."
+opam --root opam_repo init
+eval `opam --root opam_repo config -env`
+
+echo "install new compiler "
+opam --root opam_repo switch 3.12.1
+eval `opam --root opam_repo config -env`
+
+echo "adding opam signpost repo and install required packages..."
+opam --root opam_repo remote -kind git -add signpostd \
+  git://github.com/crotsos/opam-repo-dev.git
+opam --root opam_repo install ssl lwt cmdliner
+opam --root opam_repo install dns.1.0.1
+
+echo "FInally, fetch and compile test tool...."
+git clone git://github.com/crotsos/signpost-test-tool.git
+make -C signpost-test-tool
+
+echo "Running test..."
+cd signpost-test-tool/ 
+sudo ./test.native
+cd ../
